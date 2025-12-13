dc2ef07403ab0198b897aaed50f7b68af2332a1e
arthurstx <arthurteixeira.git@gmail.com>
2025-12-13 20:44:21 -0300

    refactor: adopt factory pattern for repository initialization

---
+++ src/services/factories/make-authenticate-use-case.ts
@@ -0,0 +1,8 @@
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository.js'
+import { AuthenticateUseCase } from '../authenticate.js'
+
+export function makeAuthenticateUseCase() {
+  const userRepository = new PrismaUsersRepository()
+  const authenticateUseCase = new AuthenticateUseCase(userRepository)
+  return authenticateUseCase
+}
