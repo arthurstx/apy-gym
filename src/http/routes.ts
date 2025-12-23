929bf00425cb4017a798345a00218ea3e28bb0d7
arthurstx <arthurteixeira.git@gmail.com>
2025-12-23 20:09:01 -0300

    feat: add verify jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controller/register.js'
 import { authenticate } from './controller/authenticate.js'
+import { porfile } from './controller/porfile.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/register', register)
   app.post('/authenticate', authenticate)
+
+  /**Authenticated */
+  app.get('/me', porfile)
 }
