ab104d32f185004778a3fd20aa022add13e8b0fc
arthurstx <arthurteixeira.git@gmail.com>
2025-12-24 09:57:48 -0300

    test: add authentication e2e test

---
+++ src/http/controller/authenticate.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '../../app.js'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close
+  })
+
+  it('should be able to register', async () => {
+    const email = 'email11@example.com'
+    const password = '123456'
+
+    await request(app.server).post('/register').send({
+      name: 'nome',
+      email,
+      password,
+    })
+
+    const response = await request(app.server).post('/authenticate').send({
+      email,
+      password,
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({ token: expect.any(String) })
+  })
+})
